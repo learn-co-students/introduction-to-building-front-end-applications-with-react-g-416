056acbc4846d71b6f0bbf6f3279067f77c8303ec
github username <github email address>
2018-05-01 14:10:19 -0700

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 
 
-describe('index', function() {
-  it('runs', function() {
-    expect(true).toEqual(true)
-  })
-})
+// describe('index', function() {
+//   it('runs', function() {
+//     expect(true).toEqual(true)
+//   })
+// })
