ff661e8c000436bf929e00b2bad54c1c5b487848
Laura Berge <lauraeberge@gmail.com>
2020-02-11 14:57:14 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -5,3 +5,21 @@ describe('index', function() {
     expect(true).toEqual(true)
   })
 })
+
+describe('app', function() {
+  it('exists', function() {
+    expect(app).toEqual("I don't do much.")
+  })
+})
+
+describe('me', function() {
+  it('is me', function() {
+    expect(me).toEqual("Laura")
+  })
+})
+
+describe('me', function() {
+  it('is not Emily', function() {
+    expect(me).toNotEqual("Emily")
+  })
+})
